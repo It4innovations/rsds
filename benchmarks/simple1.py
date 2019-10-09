3b2996336a715473b2b9c33c432b6c24e90eb08f
Stanislav Bohm <spirali@kreatrix.org>
2019-10-09 15:16:38 +0200

    Benchmark

---
+++ benchmarks/simple1.py
@@ -0,0 +1,13 @@
+from dask.distributed import Client
+from dask import delayed
+from dask.distributed import wait
+
+client = Client("tcp://localhost:7070")
+
+@delayed
+def do_something(x):
+    return x * 10
+
+xs = [do_something(x) for x in range(20000)]
+result = merge(xs)
+print(result.compute())
