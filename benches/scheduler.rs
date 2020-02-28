a1098110a33ffb757a9206faa4e4cb775a3027c5
Jakub Beránek <jakub.beranek@vsb.cz>
2020-02-28 11:26:22 +0100

    BENCH: add scheduler add tasks benchmark

---
+++ benches/scheduler.rs
@@ -0,0 +1,110 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use rsds::scheduler::schedproto::{TaskInfo, WorkerInfo, TaskUpdate, TaskUpdateType};
+use rsds::scheduler::workstealing::Scheduler;
+use rsds::scheduler::ToSchedulerMessage;
+
+fn create_scheduler(workers: u64) -> Scheduler {
+    let mut scheduler = Scheduler::new();
+    let workers = create_workers(workers);
+    scheduler.update(workers);
+    scheduler
+}
+
+fn new_tasks(count: u64) -> Vec<ToSchedulerMessage> {
+    (0..count)
+        .map(|i| {
+            ToSchedulerMessage::NewTask(TaskInfo {
+                id: i as u64,
+                inputs: vec![],
+            })
+        })
+        .collect()
+}
+
+fn create_workers(count: u64) -> Vec<ToSchedulerMessage> {
+    (0..count)
+        .map(|i| {
+            ToSchedulerMessage::NewWorker(WorkerInfo {
+                id: i as u64,
+                n_cpus: 1,
+                hostname: format!("worker-{}", i),
+            })
+        })
+        .collect()
+}
+
+fn remove_tasks(count: u64) -> Vec<ToSchedulerMessage> {
+    (0..count)
+        .map(|i| ToSchedulerMessage::RemoveTask(i as u64))
+        .collect()
+}
+
+fn finish_tasks(count: u64, worker_count: u64) -> Vec<ToSchedulerMessage> {
+    (0..count)
+        .map(|i| ToSchedulerMessage::TaskUpdate(TaskUpdate{
+            id: i,
+            state: TaskUpdateType::Finished,
+            worker: i % worker_count,
+            size: Some(i)
+        }))
+        .collect()
+}
+
+pub fn update(c: &mut Criterion) {
+    for i in &[1000, 5000, 10000] {
+        c.bench_with_input(BenchmarkId::new("New tasks", i), i, |b, &i| {
+            b.iter_with_setup(
+                || (create_scheduler(4), new_tasks(i)),
+                |(mut sched, tasks)| sched.update(tasks),
+            )
+        });
+        c.bench_with_input(BenchmarkId::new("Schedule", i), i, |b, &i| {
+            b.iter_with_setup(
+                || {
+                    let mut scheduler = create_scheduler(4);
+                    scheduler.update(new_tasks(i));
+                    scheduler
+                },
+                |mut sched| sched.schedule(&mut Default::default()),
+            )
+        });
+        /*c.bench_with_input(BenchmarkId::new("Finish tasks", i), i, |b, &i| {
+            b.iter_with_setup(
+                || {
+                    let mut scheduler = create_scheduler(4);
+                    scheduler.update(new_tasks(i));
+                    (scheduler, finish_tasks(i, 4))
+                },
+                |(mut sched, tasks)| sched.update(tasks),
+            )
+        });
+        c.bench_with_input(BenchmarkId::new("Remove tasks", i), i, |b, &i| {
+            b.iter_with_setup(
+                || {
+                    let mut scheduler = create_scheduler(4);
+                    scheduler.update(new_tasks(i));
+                    scheduler.update(finish_tasks(i, 4));
+                    let tasks = remove_tasks(i);
+                    (scheduler, tasks)
+                },
+                |(mut sched, tasks)| sched.update(tasks),
+            )
+        });
+        c.bench_with_input(BenchmarkId::new("Remove tasks backwards", i), i, |b, &i| {
+            b.iter_with_setup(
+                || {
+                    let mut scheduler = create_scheduler(4);
+                    scheduler.update(new_tasks(i));
+                    scheduler.update(finish_tasks(i, 4));
+                    let mut tasks = remove_tasks(i);
+                    tasks.reverse();
+                    (scheduler, tasks)
+                },
+                |(mut sched, tasks)| sched.update(tasks),
+            )
+        });*/
+    }
+}
+
+criterion_group!(scheduler, update);
+criterion_main!(scheduler);
