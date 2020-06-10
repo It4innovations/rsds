edcc96ca09de3a40ef401b51eca612f8c54c5fca
Stanislav Bohm <spirali@kreatrix.org>
2020-06-10 21:31:56 +0200

    WIP

--- src/server/taskarray.rs
+++ src/server/taskarray.rs
@@ -1,16 +1,20 @@
+use crate::scheduler::TaskId;
+use serde_json::error::ErrorCode::EofWhileParsingValue;
+
+type Int = i32;
 
 #[derive(Debug)]
 pub enum IntExpr {
     Index,
-    Const(i64),
-    Add(Box<IntExpr>, Box<IntExpr>),
-    Mul(Box<IntExpr>, Box<IntExpr>),
+    Const(Int),
+    Add(Box<(IntExpr, IntExpr)>),
+    Mul(Box<(IntExpr, IntExpr)>),
 }
 
 pub enum RangeExpr {
-    Get(IntExpr),
-    Slice(IntExpr, IntExpr, IntExpr),
-    All,
+    Get(IntExpr), // [x]
+    Slice(IntExpr, IntExpr, IntExpr), // [start:end:step]
+    All, // [:]
 }
 
 pub enum ArgumentExpr {
@@ -21,41 +25,83 @@ pub enum ArgumentExpr {
     //ObjectList(Vec<Vec<u8>>, RangeExpr),
 }
 
+pub struct TaskArrayPart {
+    size: Int,
+    function: Vec<u8>,
+    arguments: Vec<ArgumentExpr>
+}
+
+pub struct TaskArray {
+    key: String,
+    parts: Vec<TaskArrayPart>,
+    size: Int,
+}
+
+pub struct CompactGraph {
+    array: Vec<TaskArray>,
+}
+
+impl CompactGraph {
+
+    pub fn materialize(&self, mut id_counter: TaskId) {
+        for array in self.arrays {
+            self.materialize_array(array, id_counter);
+            id_counter += array.size;
+        }
+    }
+
+    pub fn materialize_array(&self, array: &TaskArray, mut id_counter: TaskId) {
+        let index = 0;
+        for part in &array.parts {
+            for i in &part.size {
+                let context = EvalContext::new(index);
+                context.eval_arg()
+                id_counter += 1;
+                index += 1;
+            }
+        }
+    }
+}
+
 pub enum Argument {
-    Int(i64),
+    Int(i32),
     TaskKey(String),
-    Object(Vec<u8>),
+    Serialized(Vec<u8>),
     List(Vec<Argument>),
 }
 
 pub struct EvalContext {
-    index: i64,
+    index: Int,
 }
 
 impl EvalContext {
 
-    pub fn new(index: i64) -> Self {
+    pub fn new(index: Int) -> Self {
         EvalContext { index }
     }
 
-    pub fn eval_int(&self, expr: &IntExpr) -> i64 {
+    pub fn eval_int(&self, expr: &IntExpr) -> Int {
         match expr {
             IntExpr::Index => self.index,
-            IntExpr::Const(v) => v,
-            IntExpr::Add(e1, e2) => self.eval_int(e1) + self.eval_int(e2),
-            IntExpr::Mul(e1, e2) => self.eval_int(e1) * self.eval_int(e2),
+            IntExpr::Const(v) => *v,
+            IntExpr::Add(pair) => self.eval_int(&pair.0) + self.eval_int(&pair.1),
+            IntExpr::Mul(pair) => self.eval_int(&pair.0) * self.eval_int(&pair.1),
         }
     }
 
     pub fn eval_arg(&self, expr: &ArgumentExpr) -> Argument {
         match expr {
             ArgumentExpr::Int(e) => Argument::Int(self.eval_int(e)),
-            ArgumentExpr::Object(data) => Argument::Object(data.clone()),
-            Argument::Task(key) => Argument::TaskKey(key.clone()),
-            Argument::TaskArray(key, RangeExpr::Single(e)) => {
+            ArgumentExpr::Object(data) => Argument::Serialized(data.clone()),
+            ArgumentExpr::Task(key) => Argument::TaskKey(key.clone()),
+            ArgumentExpr::TaskArray(key, RangeExpr::Get(e)) => {
                 let index = self.eval_int(e);
                 Argument::TaskKey(format!("{}-{}", key, index))
             }
+            ArgumentExpr::TaskArray(key, _) => {
+                todo!()
+            }
         }
     }
+
 }
